ec4702d0a3b61882d43a7848237d8eb071d94381
vadim-sobinin <vadimsobinin@yandex.ru>
2022-10-29 11:54:17 +0300

    fix RegExp

--- src/modules/inputsControl.js
+++ src/modules/inputsControl.js
@@ -4,7 +4,7 @@ const inputsControl = () => {
   const calcBlock = document.querySelector(".calc-block");
 
   calcBlock.querySelectorAll(".calc-item").forEach((element) => {
-    element.addEventListener("input", (event) => {
+    element.addEventListener("blur", (event) => {
       event.target.value = event.target.value.replace(/\D+/, "");
     });
   });
@@ -13,38 +13,48 @@ const inputsControl = () => {
 
   document
     .querySelector('input[placeholder="Your message"]')
-    .addEventListener("input", (event) => {
+    .addEventListener("blur", (event) => {
+      event.target.value = checkSpasesHyphens(event.target.value);
       event.target.value = event.target.value.replace(/([^а-яА-Я\-\ ]+)/, "");
     });
 
 
+  const checkSpasesHyphens = (input) => {
+    input = (input.replace(/ {2,}/g," "));
+    input = (input.replace(/-{2,}/g,"-"));
+    input = input.replace(/^ +| +$|( ) +/g, "$1");
+    input = input.replace(/^-+|-+$/gm, "");
+    return input;
+  };
+
   const nameForms = document.querySelectorAll("input[placeholder='Your name']");
   nameForms.forEach((element) => {
     element.addEventListener("blur", (event) => {
-      let correctedInput = (event.target.value.replace(/ {2,}/g," "));
-      correctedInput = (correctedInput.replace(/-{2,}/g,"-cv"));
-      correctedInput = correctedInput.replace(/[^а-яА-ЯËё\-\ ]/g, "");
-      correctedInput = correctedInput.replace(/^ +| +$|( ) +/g, "$1");
-      correctedInput = correctedInput.replace(/^-+|-+$/gm, "");
+
+      let correctedInput = event.target.value.replace(/[^а-яА-ЯËё\-\ ]/g, "");
+
+      correctedInput = checkSpasesHyphens(correctedInput);
       
+      if (correctedInput != "") {
       correctedInput = correctedInput.replace(/( |^)[а-яА-ЯËё]/g, function(letter){ return letter.toUpperCase(); });
       
        let newCorrectedInput = correctedInput.split(" ").map((word) => {
         return word.split("").reduce((total, letter) => total + letter.toLowerCase());
       });
 
-      
       event.target.value = newCorrectedInput.join(" ");
-
-
+    } else {
+      event.target.value = "";
+    }
     });
   });
 
   const emailForms = document.querySelectorAll(".form-email");
   emailForms.forEach((element) => {
-    element.addEventListener("input", (event) => {
+    element.addEventListener("blur", (event) => {
+      event.target.value = checkSpasesHyphens(event.target.value);
       event.target.value = event.target.value.replace(
-        /([^\w\@\-\_\.\!\~\*\']+)/,
+        /([^\w\@\-\_\.\!\~\*\']+)/g,
         ""
       );
     });
@@ -52,8 +62,9 @@ const inputsControl = () => {
 
   const phoneForms = document.querySelectorAll(".form-phone");
   phoneForms.forEach((element) => {
-    element.addEventListener("input", (event) => {
-      event.target.value = event.target.value.replace(/([^\d\-\(\)]+)/, "");
+    element.addEventListener("blur", (event) => {
+      event.target.value = event.target.value.replace(/([^\d\-\(\)]+)/g, "");
+      event.target.value = checkSpasesHyphens(event.target.value);
     });
   });
 };
